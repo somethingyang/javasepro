00d2c3f4a9adac5b2540d251490f35fd36a7917b
yuan <2086897602@qq.com>
2023-04-04 21:45:59 +0800

    面向对象 封装

--- hello/src/com/yqy/day4/Demo10.java
+++ hello/src/com/yqy/day4/Demo10.java
@@ -5,7 +5,7 @@ public class Demo10 {
         Student s1 = new Student();
         s1.name = "锐文一";
         s1.age = 18;
-        s1.score = 59;;
+        s1.score = 59;
         s1.study();
         s1.study();
         s1.study();
